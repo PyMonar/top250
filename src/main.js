57d06f6dd23d4478f730667c4136a453dadfc6a8
PengYi <pengyi@zhihu.com>
2018-11-21 11:12:36 +0800

    remove sentry

--- src/main.js
+++ src/main.js
@@ -4,15 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import Raven from 'raven-js';
-import RavenVue from 'raven-js/plugins/vue';
-
-Raven
-    .config('https://57621f62e5bf4b3d92fe1ed232ba3e4a@sentry.io/1301139')
-    .addPlugin(RavenVue, Vue)
-    .install();
-
-Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
